3e9195906088bc61d9fc20b9ac09a40228ee65ba
pareeksachin77 <pareeksachin77@gmail.com>
2022-12-06 17:04:30 +0530

    cabinvoice genrator

---
+++ CabInvoiceGenrator/Ride.cs
@@ -0,0 +1,13 @@
+﻿namespace CabInvoiceGenrator
+{
+    public class Ride
+    {
+        pubilc double distance;
+        public int time;
+        public Ride(double distance, int time)
+        {
+            this.distance = distance;
+            this.time = time;
+        }
+    }
+}
